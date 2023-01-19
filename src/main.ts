ebd8503b578f7945ccc80df88fb1abad579e6df1
Yinan Li <gabriel.li5566@gmail.com>
2023-01-20 01:07:06 +0200

    add global objects

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,25 @@
+import axios from "axios";
+import qs from "qs";
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
-createApp(App).use(store).use(router).mount("#app");
+const app = createApp(App);
+// 挂载全局对象
+app.config.globalProperties.fool = "Hello World";
+app.config.globalProperties.$axios = axios;
+axios.defaults.baseURL = "http://bufantec.com/api/";
+// 请求拦截器
+axios.interceptors.request.use(
+  (config) => {
+    if (config.method?.toLocaleLowerCase() == "post") {
+      config.data = qs.stringify(config.data);
+    }
+    return config;
+  },
+  (err) => {
+    return Promise.reject(err);
+  }
+);
+app.use(store).use(router).mount("#app");
