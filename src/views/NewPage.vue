31ff6a2ad8a248afbd1c27f733e9a4ba3817d449
Yinan Li <gabriel.li5566@gmail.com>
2023-01-19 23:51:12 +0200

    add emit and expose functions

---
+++ src/views/NewPage.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h3>Son Component</h3>
+    <button @click="sendInfo">Send</button>
+  </div>
+</template>
+<script lang="ts" setup>
+import { defineEmits, defineExpose } from "vue";
+let info = "The info from son component";
+let exposeInfo = "The exposeInfo from som component";
+const emit = defineEmits(["on-click"]);
+
+function sendInfo() {
+  emit("on-click", info, true);
+}
+
+defineExpose({
+  exposeInfo,
+});
+</script>
