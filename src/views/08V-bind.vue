5bfffa14f6a397eff47564d8de354845f7c5026c
Yinan Li <gabriel.li5566@gmail.com>
2023-01-24 17:53:10 +0200

    update vi-bind

---
+++ src/views/08V-bind.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h3>06 V-bind</h3>
+    <p tag="title"></p>
+    <img v-bind:src="image" alt="" />
+    <a :href="url">Google</a>
+  </div>
+</template>
+<script setup lang="ts">
+import { ref } from "vue";
+const image = ref(require("../assets/logo.png"));
+const url = ref("http://www.google.com");
+</script>
+<style></style>
